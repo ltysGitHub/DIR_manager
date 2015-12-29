5735bf270c7c5d3a54c24495d8557b05dfc13776
root <root@lty-ubuntu.(none)>
2015-12-29 22:08:21 +0800

    第一个文件,实现文件夹内容读取

---
+++ dir_func.php
@@ -0,0 +1,20 @@
+<?php
+function readDirectory($path){
+	$handle = opendir($path);
+	while(($item=readdir($handle))!==false){
+		if($item=="."||$item=="..")
+			continue;
+		else if(is_file($path.'/'.$item)){
+			$arr["file"][]=$item;
+		}
+		else if(is_dir($path.'/'.$item)){
+			$arr["dir"][]=$item;
+		}
+	}
+	closedir($handle);
+	return $arr;
+}
+
+print_r(readDirectory("testdir"));
+
+?>
